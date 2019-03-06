7707faba6a09bf407cde180c669ccdaf74502f6c
ADMIN <vishnushramaksg34@gmail.com>
2019-03-06 13:26:54 +0530

    GithuIntegration

--- app/src/main/java/com/tech_rs/githubapi/Fragment_repository.java
+++ app/src/main/java/com/tech_rs/githubapi/Fragment_repository.java
@@ -1,5 +1,7 @@
 package com.tech_rs.githubapi;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,15 +9,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Fragment_repository extends Fragment {
     View v;
+    private Button button;
     public Fragment_repository() {
     }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v=inflater.inflate(R.layout.repository,container,false);
+        button=v.findViewById(R.id.search);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/"));
+                startActivity(i);
+            }
+        });
         return v;
     }
 }
