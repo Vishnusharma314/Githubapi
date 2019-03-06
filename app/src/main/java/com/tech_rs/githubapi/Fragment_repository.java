f2f1571af340610de0fe4fff1a9c78524e55b807
ADMIN <vishnushramaksg34@gmail.com>
2019-03-06 12:40:29 +0530

    calling

---
+++ app/src/main/java/com/tech_rs/githubapi/Fragment_repository.java
@@ -0,0 +1,21 @@
+package com.tech_rs.githubapi;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_repository extends Fragment {
+    View v;
+    public Fragment_repository() {
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v=inflater.inflate(R.layout.repository,container,false);
+        return v;
+    }
+}
